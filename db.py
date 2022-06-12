ca26b644ff8e42b88365b6c5d9e5004723800e9b
Roman Belchickov <lol2497@bk.ru>
2022-06-12 07:59:55 +0300

    db server and table users

---
+++ db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+db = sqlite3.connect('server.db')
+sql = db.cursor()
+
+sql.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        login TEXT,
+        password TEXT,
+        token TEXT
+    );
+""")
+
+db.commit()
+
+sql.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (1, 'User', 'qwe', 'QwE1'))
+db.commit()

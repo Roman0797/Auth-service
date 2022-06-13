fdf074eab1348b65d0a8891854b2db89857e0f0a
Roman Belchickov <lol2497@bk.ru>
2022-06-13 16:08:27 +0300

    final

--- db.py
+++ db.py
@@ -1,7 +1,31 @@
+import jwt, datetime, time
+from flask import jsonify, g
 import sqlite3
 
+
+SECRET_KEY = "bearer token"
 db = sqlite3.connect('server.db')
 sql = db.cursor()
+
+
+def encode_auth_token():
+    try:
+        payload = {
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=2, seconds=10),
+            'iat': datetime.datetime.utcnow(),
+            'iss': 'User_2',
+            'data': {
+                'id': 3,
+            }
+        }
+        return jwt.encode(
+            payload,
+            SECRET_KEY,
+            algorithm='HS256'
+        )
+    except Exception as e:
+        return e
+
 
 sql.execute("""
     CREATE TABLE IF NOT EXISTS users (
@@ -15,5 +39,11 @@ sql.execute("""
 
 db.commit()
 
-sql.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?)", (1, 'User', 'qwe', 'QwE1', '127.0.0.1'))
+sql.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?)", (3, 'User_2', 'qwe_2', encode_auth_token(), '127.0.0.3'))
 db.commit()
+
+sqlite_select_query = """SELECT * from users"""
+cursor = db.cursor()
+cursor.execute(sqlite_select_query)
+records = cursor.fetchall()
+print(records)
--- main.py
+++ main.py
@@ -1,43 +1,75 @@
 import json
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, g, redirect
 import sqlite3
+import time
+import jwt, datetime, time
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
+SECRET_KEY = "bearer token"
 db = sqlite3.connect('server.db', check_same_thread=False)
 sql = db.cursor()
 
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+)
 
-# sql.execute("SELECT token FROM users")
-# print(sql.fetchall())
+users = dict()
+
 
 @app.route('/', methods=['POST'])
+@limiter.limit("3/minute")
 def authorize_user():
-    data = json.loads(request.data)
-    print(request.remote_addr)
-    success = verify_token(request.headers.get('Authorization'))
+    token = request.headers.get('Authorization').split()[1]
+    success = verify_token(token)
+    users[request.remote_addr] = success
     if success:
         data = {'message': 'Done', 'code': 'SUCCESS'}
         return make_response(jsonify(data), 200)
     else:
         return make_response(
             jsonify(
-                {"message": "Неверный логин или пароль"}
+                {"message": "Пожалуйста, передайте правильную информацию заголовка подтверждения"}
             ),
             401,
         )
 
 
+@app.route('/test', methods=['GET'])
+def auth_test():
+    message = {'message': 'Authorized', 'code': 'SUCCESS'} if users.get(request.remote_addr) else {
+        "message": "Пожалуйста, передайте правильную информацию заголовка подтверждения"}
+    response_code = 200 if users.get(request.remote_addr) else 401
+    return make_response(jsonify(message), response_code)
+
+
 def verify_token(token):
-    token = token.split(" ")[1]
-    sql.execute(f"SELECT token FROM users WHERE token = '{token}'")
-    token_db = sql.fetchone()
-    if token_db:
-        token_db = token_db[0]
-    else:
-        token_db = False
-    if token == token_db:
-        return True
-    return False
+    token = decode_auth_token(auth_token=token)
+    if token == 'Invalid Token ' or token == 'Token expired ':
+        return False
+    user_id = token['data']['id']
+    sql.execute(f"SELECT id FROM users WHERE id = '{user_id}'")
+    id_user = sql.fetchone()
+
+    if id_user is None:
+        return False
+
+    return True
+
+
+def decode_auth_token(auth_token):
+    try:
+        payload = jwt.decode(auth_token, SECRET_KEY, algorithms=['HS256'], options={'verify_exp': False})
+        if 'data' in payload and 'id' in payload['data']:
+            return payload
+        else:
+            raise jwt.InvalidTokenError
+    except jwt.ExpiredSignatureError:
+        return 'Token expired '
+    except jwt.InvalidTokenError:
+        return 'Invalid Token '
 
 
 app.run(debug=True)
